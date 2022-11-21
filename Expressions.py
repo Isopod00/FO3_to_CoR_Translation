c95bba7583708ba9696fac20812bdafadd598c5f
Anthony Brogni <anthonybrog@gmail.com>
2022-11-21 17:07:56 -0600

    minor updates

--- Expressions.py
+++ Expressions.py
@@ -1,57 +1,56 @@
 class Negation:
-    """ This class describes the mathematical symbol ¬
-    The argument for this class can be any logical expression that the negation should be applied to"""
+    """ This class describes the mathematical symbol ¬ (not/negation)
+    The argument for this class can be any logical expression that the negation should be applied to. """
 
     def __init__(self, arg):
         self.argument = arg
 
-    def _negate_(self):
+    def _negate(self):
         return self.argument
 
-    def _negation_normal_form_(self):
-        return self.argument._negate_()
+    def _negation_normal_form(self):
+        return self.argument._negate()
 
-    def __str__(self):
-        return f'¬({str(self.argument)})'
+    def __str__(self) -> str:
+        return f'¬({self.argument})'
 
 
 class ForAll:
-    """ This class describes the mathematical symbol ∀.
+    """ This class describes the mathematical symbol ∀ (for all)
          The two arguments (var and arg) describe the variable in question and
-         the expression that ∀var is being applied to, respectively."""
+         the expression that ∀var is being applied to, respectively. """
 
     def __init__(self, var, arg):
         self.variable = var
         self.argument = arg
 
-    def _negate_(self):
-        return ThereExists(self.variable, self.argument._negate_())
+    def _negate(self):
+        return ThereExists(self.variable, self.argument._negate())
 
-    def _negation_normal_form_(self):
+    def _negation_normal_form(self):
         return ForAll(self.variable, self.argument._negation_normal_form())
 
-    def __str__(self):
-        return f'∀{self.variable}. {str(self.argument)}'
+    def __str__(self) -> str:
+        return f'∀{self.variable}. {self.argument}'
 
 
 class ThereExists:
-    """ This class describes the mathematical symbol ∃.
+    """ This class describes the mathematical symbol ∃ (there exists)
      The two arguments (var and arg) describe the variable in question and
-     the expression that ∃var is being applied to, respectively."""
+     the expression that ∃var is being applied to, respectively. """
 
     def __init__(self, var, arg):
         self.variable = var
         self.argument = arg
 
-    def _negate_(self):
-        return ForAll(self.variable, self.argument._negate_())
+    def _negate(self):
+        return ForAll(self.variable, self.argument._negate())
 
-    def _negation_normal_form_(self):
-        pass
-        # TODO
+    def _negation_normal_form(self):
+        return ThereExists(self.variable, self.argument._negation_normal_form())
 
-    def __str__(self):
-        return f'∃{self.variable}. {str(self.argument)}'
+    def __str__(self) -> str:
+        return f'∃{self.variable}. {self.argument}'
 
 
 class AND:
@@ -61,11 +60,11 @@ class AND:
         self.argument1 = arg1
         self.argument2 = arg2
 
-    def _negate_(self):
-        return OR(self.argument1._negate_(), self.argument2._negate_())
+    def _negate(self):
+        return OR(self.argument1._negate(), self.argument2._negate())
 
-    def __str__(self):
-        return f'({self.argument1}) \/ ({self.argument2})'
+    def __str__(self) -> str:
+        return f'({self.argument1}) ∧ ({self.argument2})'
 
 
 class OR:
@@ -75,11 +74,12 @@ class OR:
         self.argument1 = arg1
         self.argument2 = arg2
 
-    def _negate_(self):
-        return AND(self.argument1._negate_(), self.argument2._negate_())
+    def _negate(self):
+        return AND(self.argument1._negate(), self.argument2._negate())
 
-    def __str__(self):
-        return f'({self.argument1}) /\ ({self.argument2})'
+    def __str__(self) -> str:
+        return f'({self.argument1}) ∨ ({self.argument2})'
+
 
 class Equals:
     """ This class describes two arguments being equal to each other.
@@ -89,11 +89,17 @@ class Equals:
         self.argument1 = arg1
         self.argument2 = arg2
 
-    def _negate_(self):
+    def _negate(self):
         return Negation(self)
+
+    def __str__(self) -> str:
+        return f'{self.argument1} = {self.argument2}'
 
 
 # This code only runs if this file is run directly (it doesn't run when imported as a library)
 if __name__ == "__main__":
     expression = Negation(ForAll("x", AND(Equals("x", "x"), Equals("x", "x"))))
-    expression._negation_normal_form_()
+
+    print("Original Expression:", expression)  # Original expression
+    print("Negated Expression:", expression._negate())  # Negated expression
+    print("Negation Normal Form:", expression._negation_normal_form())  # Negation Normal Form
